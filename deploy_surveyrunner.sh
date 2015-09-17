60e162a543af929a729f7714e9ecf1b096f76ca9
Dan Hilton <daniel.hilton@gmail.com>
2015-09-17 11:41:30 +0100

    SurveyRunner deploy now is branch aware.
    To enable us to deploy test branches in the future, the deployment
    process now is branch away for both projects.

--- deploy_surveyrunner.sh
+++ deploy_surveyrunner.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 SRUNNER_REPO_URL=https://github.com/ONSdigital/eq-survey-runner
+BRANCH=master
 
+echo $SRUNNER_REPO_URL
 mkdir -p tmp
 cd ./tmp
 git clone $SRUNNER_REPO_URL
 cd ./eq-survey-runner
+git fetch
+git checkout $BRANCH
 heroku git:remote -a $1
-git push heroku master
+git push heroku $BRANCH:master
